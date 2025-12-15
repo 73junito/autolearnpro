8eddc9fdedaee6c3445c02ba1fc96bb457cd5336
73junito <73junito@gmail.com>
2025-12-15 15:22:48 -0600

    chore(ci): force mix compile in Dockerfile for CI build

---
+++ D:/Automotive and Diesel LMS/backend/lms_api/lib/lms_api_web.ex
@@ -0,0 +1,83 @@
+defmodule LmsApiWeb do
+  @moduledoc false
+
+  def controller do
+    quote do
+      use Phoenix.Controller,
+        namespace: LmsApiWeb
+
+      import Plug.Conn
+      alias LmsApiWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/lms_api_web/templates",
+        namespace: LmsApiWeb
+
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1]
+
+      alias LmsApiWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  def router do
+    quote do
+      use Phoenix.Router
+      import Plug.Conn
+      import Phoenix.Controller
+    end
+  end
+
+  def channel do
+    quote do
+      use Phoenix.Channel
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
+defmodule LmsApiWeb do
+  def controller do
+    quote do
+      use Phoenix.Controller,
+        formats: [:json],
+        layouts: []
+
+      import Plug.Conn
+      alias LmsApiWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/lms_api_web/templates",
+        namespace: LmsApiWeb
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+
+      # Include shared imports and aliases for views
+      import LmsApiWeb.ErrorHelpers
+      alias LmsApiWeb.Router.Helpers, as: Routes
+    end
+  end
+
+  def router do
+    quote do
+      use Phoenix.Router
+      import Plug.Conn
+      import Phoenix.Controller
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
